6a0873369e9d15f8c60f45f19a419528640f41f2
Rachel Beckstead <rachelbeckstead@Rachels-Air.lan>
2020-04-24 12:57:06 -0600

    added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Courses.create(name: "Breakfast")
+Courses.create(name:"Lunch")
+Courses.create(name:"Dinner")
+Courses.create(name:"Dessert")
+Genres.create(name: "Mexican")
+Genres.create(name: "Italian")
+Genres.create(name: "American")
+Genres.create(name: "Chinese")
+Genres.create(name: "Hawaiian")
+Genres.create(name: "Korean")
+Genres.create(name: "Indian")
+Genres.create(name: "Polynesian")
+Genres.create(name: "Japanese")
